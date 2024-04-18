1e46e7c8c365d3431433e64a33890db2cf5738ac
Mohamed Medhat <mmedhat1910@gmail.com>
2024-04-18 14:18:38 +0200

    project init

---
+++ src/main/java/com/massivelyscalableteam/scalablejavausersmodule/ScalableJavaUsersModuleApplication.java
@@ -0,0 +1,13 @@
+package com.massivelyscalableteam.scalablejavausersmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableJavaUsersModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableJavaUsersModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/massivelyscalableteam/scalablejavausersmodule/ScalableJavaUsersModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massivelyscalableteam.scalablejavausersmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableJavaUsersModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
